83e3e520db47739c9e2ddc1f0babda80b9fad277
Des Herriott <des.herriott@gmail.com>
2013-07-01 12:25:44 +0100

    Better block selection for the slide initiator wand. Use expanding cuboid layers centred on the selected block instead of just scanning the whole cuboid from the min x,y,z upwards.

--- src/main/java/me/desht/landslide/SlideOTron.java
+++ src/main/java/me/desht/landslide/SlideOTron.java
@@ -9,6 +9,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import me.desht.dhutils.MiscUtil;
+import me.desht.dhutils.cuboid.Cuboid;
+import me.desht.dhutils.cuboid.Cuboid.CuboidDirection;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -25,7 +27,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.BlockIterator;
 
 public class SlideOTron {
-	private static final String DISPLAY_PREFIX = ChatColor.YELLOW + "Slide-O-Tron: " + ChatColor.GOLD;
+	private static final String DISPLAY_PREFIX = ChatColor.YELLOW + "Slide-O-Tron\u2122: " + ChatColor.GOLD;
 	private static final String POWER_PREFIX = ChatColor.ITALIC.toString();
 	private static final Material WAND_MATERIAL = Material.BLAZE_ROD;
 
@@ -208,12 +210,6 @@ public class SlideOTron {
 				}
 				n++;
 			}
-//			b = player.getTargetBlock(transparent, 140);
-//			if (b != null && b.getY() != 0 && b.getType() != Material.AIR) {
-//				forceSlide(plugin, player, b);
-//			} else {
-//				player.playSound(player.getLocation(), Sound.NOTE_BASS, 1.0f, 1.0f);
-//			}
 			break;
 		default:
 			break;
@@ -221,25 +217,43 @@ public class SlideOTron {
 	}
 
 	private void forceSlide(LandslidePlugin plugin, Player player, Block b) {
-		int x0 = b.getX(), y0 = b.getY(), z0 = b.getZ();
-		World w = b.getWorld();
 		int size = Math.min(power, 20);
 		int n = 0;
-		for (int x = x0 - size; x <= x0 + size; x++) {
-			for (int y = y0 - size; y <= y0 + size; y++) {
-				for (int z = z0 - size; z <= z0 + size; z++) {
-					Block b1 = w.getBlockAt(x, y, z);
-					if (b1.getType() == b.getType()) {
-						BlockFace face = plugin.getSlideManager().wouldSlide(b1);
-						if (face != null) {
-							plugin.getSlideManager().scheduleBlockSlide(b1, face, b1.getType(), b1.getData(), false);
-							n++;
-						}
-					}
+
+		Cuboid c = new Cuboid(b.getLocation());
+		for (int s = 0; s <= size; s++) {
+			if (s == 0) {
+				// degenerate case
+				BlockFace face = plugin.getSlideManager().wouldSlide(b);
+				if (face != null) {
+					plugin.getSlideManager().scheduleBlockSlide(b, face);
+					n++;
 				}
+			} else {
+				n += forceSlide(plugin, b.getType(), c.getFace(CuboidDirection.Down));
+				n += forceSlide(plugin, b.getType(), c.getFace(CuboidDirection.North).inset(CuboidDirection.Vertical, 1));
+				n += forceSlide(plugin, b.getType(), c.getFace(CuboidDirection.South).inset(CuboidDirection.Vertical, 1));
+				n += forceSlide(plugin, b.getType(), c.getFace(CuboidDirection.East).inset(CuboidDirection.Vertical, 1));
+				n += forceSlide(plugin, b.getType(), c.getFace(CuboidDirection.West).inset(CuboidDirection.Vertical, 1));
+				n += forceSlide(plugin, b.getType(), c.getFace(CuboidDirection.Up));
 			}
+			c = c.outset(CuboidDirection.Both, 1);
 		}
 		String s = n == 1 ? "" : "s";
 		MiscUtil.statusMessage(player, "Scheduled " + n + " block" + s + " to slide");
+	}
+
+	private int forceSlide(LandslidePlugin plugin, Material mat, Cuboid c) {
+		int n = 0;
+		for (Block b : c) {
+			if (b.getType() == mat) {
+				BlockFace face = plugin.getSlideManager().wouldSlide(b);
+				if (face != null) {
+					plugin.getSlideManager().scheduleBlockSlide(b, face);
+					n++;
+				}
+			}
+		}
+		return n;
 	}
 }
