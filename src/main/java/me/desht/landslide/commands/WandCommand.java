ad56e851adc3bf492f8bc48dd64c770f75ad6391
Des Herriott <des.herriott@gmail.com>
2013-07-01 14:31:08 +0100

    Fixed arg count checking in /landslide wand command Also added some informative messages.

--- src/main/java/me/desht/landslide/commands/WandCommand.java
+++ src/main/java/me/desht/landslide/commands/WandCommand.java
@@ -1,11 +1,13 @@
 package me.desht.landslide.commands;
 
 import me.desht.dhutils.DHValidate;
+import me.desht.dhutils.MiscUtil;
 import me.desht.dhutils.commands.AbstractCommand;
 import me.desht.landslide.SlideOTron;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -22,7 +24,7 @@ public class WandCommand extends AbstractCommand {
 	public boolean execute(Plugin plugin, CommandSender sender, String[] args) {
 		Player player;
 		if (args.length > 0) {
-			String playerName = args[1];
+			String playerName = args[0];
 			player = Bukkit.getPlayer(playerName);
 			DHValidate.notNull(player, "Player " + playerName + " is not online.");
 		} else {
@@ -33,6 +35,13 @@ public class WandCommand extends AbstractCommand {
 		player.getInventory().addItem(SlideOTron.makeWand());
 		player.updateInventory();
 
+		if (!sender.getName().equals(player.getName())) {
+			MiscUtil.statusMessage(sender, "Gave a Slide-O-Tron\u2122 to " + player.getName());
+			String from = sender instanceof ConsoleCommandSender ? "the Powers-That-Be" : sender.getName();
+			MiscUtil.alertMessage(player, "You received a Slide-O-Tron\u2122 from " + from + ".  Use with care.");
+		} else {
+			MiscUtil.statusMessage(sender, "Gave yourself a Slide-O-Tron\u2122");
+		}
 		return true;
 	}
 
